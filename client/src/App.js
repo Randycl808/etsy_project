5f57f6cf669e1cffe0a7c0d8d2c4f496f252c1cc
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-03-31 15:56:28 -0700

    nav bar bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Navbar from './components/shared/NavBar';
-import { Routes, Route, useParams} from 'react-router-dom';
-import Login from './components/shared/Login';
-import Register from './components/shared/Register';
-import NoMatch from './components/shared/NoMatch';
-import FetchUser from './components/shared/FetchUser';
-import ProtectedRoute from './components/shared/ProtectRoute';
-import Products from './components/auth/Products';
-import Categories from './components/auth/Categories';
-import FindProduct from './components/auth/FindProduct';
-import Sellers from './components/auth/Sellers';
-import Charts from './components/auth/Charts';
+import "./App.css";
+import Navbar from "./components/shared/NavBar";
+import { Routes, Route, useParams } from "react-router-dom";
+import Login from "./components/shared/Login";
+import Register from "./components/shared/Register";
+import NoMatch from "./components/shared/NoMatch";
+import FetchUser from "./components/shared/FetchUser";
+import ProtectedRoute from "./components/shared/ProtectRoute";
+import Products from "./components/auth/Products";
+import Categories from "./components/auth/Categories";
+import FindProduct from "./components/auth/FindProduct";
+import Sellers from "./components/auth/Sellers";
+import Charts from "./components/auth/Charts";
 
 // Fetch user: it is going to see if the user is logged in(valid user)
 function App() {
@@ -22,19 +22,19 @@ function App() {
         <>
           <Routes>
             {/* Unprotected */}
-            <Route path='/login' element={<Login />}/>
-            <Route path='/register' element={<Register />}/>
-           
-                {/* protected in routes inside of here you need to logged in*/}
-                {/* else you go to login page*/}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* protected in routes inside of here you need to logged in*/}
+            {/* else you go to login page*/}
             <Route element={<ProtectedRoute />}>
-              <Route path='/' element={<Products />} />
-              <Route path='/categories' element={<Categories />} />
-              <Route path='/find' element={<FindProduct />} />
-              <Route path='/sellers' element={<Sellers />} />
-              <Route path='/charts' element={<Charts />} />
-            </Route>  
-            <Route path='*' element={<NoMatch />}/>
+              <Route path="/" element={<Products />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/find" element={<FindProduct />} />
+              <Route path="/sellers" element={<Sellers />} />
+              <Route path="/charts" element={<Charts />} />
+            </Route>
+            <Route path="*" element={<NoMatch />} />
           </Routes>
         </>
       </FetchUser>
