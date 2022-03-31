cc8e06a3d394f7bed622e19af703f5175f9c654e
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-03-31 16:49:08 -0700

    bootstrap nav and login

--- client/src/components/shared/Login.js
+++ client/src/components/shared/Login.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
   const [email, setEmail] = useState("test1@test.com");
@@ -18,15 +19,62 @@ const Login = () => {
         Login
         <hr></hr>
       </h1>
-      <form onSubmit={handleSubmit}>
-        <p>email</p>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
-        <p>password</p>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button>login</button>
-        <hr></hr>
-      </form>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
+        <Button
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+          variant="primary"
+          type="submit"
+        >
+          Login
+        </Button>
+      </Form>
     </div>
   );
 };
 export default Login;
+
+{
+  /* <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>; */
+}
+// {
+//   <p>email</p>
+//         <input value={email} onChange={(e) => setEmail(e.target.value)} />
+//         <p>password</p>
+//         <input value={password} onChange={(e) => setPassword(e.target.value)} />
+//         <button>login</button>
+//         <hr></hr>
+// }
