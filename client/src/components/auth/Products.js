71b76590df793ef65f64057f35bc7bb50a3db3ab
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-04-01 15:16:25 -0600

    comments added

--- client/src/components/auth/Products.js
+++ client/src/components/auth/Products.js
@@ -10,11 +10,17 @@ const Products = () => {
   }, []);
 
   const normalizeData = (rawData) => {
+    // Return all seller id's from api call in a new array
     let sellers = rawData.map((p) => p.seller_id);
+    // store unique seller id's in a new array using ... and Set
     let uniqueIds = [...new Set(sellers)];
+    // Search through each unique id
     let sellerProductData = uniqueIds.map((uid) => {
+      // Filter out products that belong to seller
       let products = rawData.filter((p) => uid == p.seller_id);
+      // Get seller info from first product
       let { first_name, last_name, email, seller_id } = products[0];
+      // Return an organized object with custom data display
       return {
         name: `${first_name} ${last_name}`,
         email: email,
@@ -22,14 +28,16 @@ const Products = () => {
         id: seller_id,
       };
     });
+    // Return the organized object
     return sellerProductData;
   };
 
   const renderRows = (products) => {
-    console.log(products);
+    console.log('product:', products);
     return products.map((p) => {
       return (
         <tr key={p.id}>
+          <td>{p.id}</td>
           <td>{p.name}</td>
           <td>{p.price}</td>
           <td>{p.description}</td>
@@ -41,16 +49,18 @@ const Products = () => {
 
   const renderData = () => {
     return sellerProducts.map((seller) => {
-      console.log(seller);
+      console.log('seller:', seller);
       return (
         <ListGroup.Item key={seller.id}>
           <div>
             <h1>{seller.name}</h1>
             <p>{seller.email}</p>
+            <p>Seller ID: {seller.id}</p>
           </div>
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
+                <th>Product ID</th>
                 <th>Name</th>
                 <th>Price</th>
                 <th>Description</th>
