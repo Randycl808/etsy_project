b43167a1f6b956b455aacfcfd117d2ada434a871
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-04 16:13:14 -0600

    Merge branch 'master' of github.com:Randycl808/etsy_project into shcategories

--- client/src/components/auth/Categories.js
+++ client/src/components/auth/Categories.js
@@ -21,43 +21,33 @@ const Categories = () => {
 
   const handleSelect = (event) => {
     let selectedCategory = event.target.value;
-
-    setFilterCategories(
-      products.filter((p) => p.category === selectedCategory)
-    );
+    
+    setFilterCategories(products.filter((p) => p.category === selectedCategory));
   };
 
   const renderSelect = (categories) => {
-    console.log(categories);
+    console.log(categories)
     return (
       <div key={categories.id} className="selectBar">
-        <Form.Select
-          defaultValue=""
-          label="Select"
-          onChange={handleSelect}
-          aria-label="Select Category"
-        >
-          <option value="" disabled hidden>
-            Please Choose...
-          </option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
+      <Form.Select defaultValue="" label='Select'  onChange={handleSelect} aria-label="Select Category">
+          <option value="" disabled hidden>Please Choose...</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>{category}</option>
+        ))}
         </Form.Select>
-      </div>
+        </div>
     );
   };
 
   const getSelect = () => {
     let uniqueCategories = getUniqueCategories();
     return renderSelect(uniqueCategories);
+   
   };
 
   const renderFilteredCategoryProperties = () => {
     if (!filteredCategories) {
-      return <h3>Please Choose a Category to continue</h3>;
+      return <p>Please Choose a Category to continue</p>;
     }
 
     return (
@@ -77,22 +67,16 @@ const Categories = () => {
               <td className="Bgc">{c.category}</td>
               <td className="Bgc">{c.price}</td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredCategories.map((c) => (
-              <tr key={c.id}>
-                <td className="">{c.category}</td>
-                <td className="">{c.price}</td>
-              </tr>
             ))}
-          </tbody>
+            
+        </tbody>
         </Table>
-      </div>
+        </div>
     );
   };
 
   return (
-    <div>
+    <div >
       <h1 className="App header headbar text-white">Categories</h1>
       {getSelect()}
       {renderFilteredCategoryProperties()}
@@ -101,4 +85,7 @@ const Categories = () => {
 };
 
 export default Categories;
+
+
+
 
--- client/src/components/shared/Login.js
+++ client/src/components/shared/Login.js
@@ -72,64 +72,8 @@ const Login = () => {
     <button type="button" className="btn btn-link btn-floating mx-1">
       {/* <i className="fab fa-github"></i> */}
     </button>
-          </div>
+        
 
-          {/* <!-- 2 column grid layout for inline styling --> */}
-          <div className="row mb-4">
-            <div className="col d-flex justify-content-center">
-              {/* <!-- Checkbox --> */}
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="form2Example31"
-                />
-                <label className="form-check-label" form="form2Example31">
-                  {" "}
-                  Remember me{" "}
-                </label>
-              </div>
-            </div>
-
-            <div className="col">
-              {/* <!-- Simple link --> */}
-              <a href="#!">Forgot password?</a>
-            </div>
-          </div>
-
-          {/* <!-- Submit button --> */}
-          <button
-            type="button"
-            className=" signin btn btn-primary btn-block mb-4"
-            onClick={(e) => {
-              handleSubmit(e);
-            }}
-          >
-            Sign in
-          </button>
-
-          {/* <!-- Register buttons --> */}
-          <div className="text-center">
-            <p>
-              Not a member? <a href="/register">Register</a>
-            </p>
-
-            <button type="button" className="btn btn-link btn-floating mx-1">
-              <i className="fab fa-facebook-f"></i>
-            </button>
-
-            <button type="button" className="btn btn-link btn-floating mx-1">
-              <i className="fab fa-google"></i>
-            </button>
-
-            <button type="button" className="btn btn-link btn-floating mx-1">
-              <i className="fab fa-twitter"></i>
-            </button>
-
-            <button type="button" className="btn btn-link btn-floating mx-1">
-              <i className="fab fa-github"></i>
-            </button>
           </div>
         </div>
       </Form>
