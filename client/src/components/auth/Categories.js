aed3f6084ba97f599395eb2775e1af0a105e32ef
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-04 15:36:18 -0600

    styles and useremail

--- client/src/components/auth/Categories.js
+++ client/src/components/auth/Categories.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useAxios from "axios-hooks";
-import { Form, Table } from "react-bootstrap";
+import {Form,Table} from "react-bootstrap";
+
 
 const Categories = () => {
   const [{ data: products, loading, error }] = useAxios("/api/products");
@@ -50,21 +51,21 @@ const Categories = () => {
     }
 
     return (
-      <div className="mainbox">
-      <Table striped bordered hover>
-        <thead>
-          <tr key={filteredCategories.id} className="Bgc">
+      <div className="mainbox box1">
+      <Table striped bordered hover className=" box1  table table-striped table-hover ">
+        <thead className="Bgc">
+          <tr key={filteredCategories.id} className="Bgc ">
             <th>Category</th>
             <th>Price</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody >
           
             {filteredCategories.map((c) => (
             
             <tr key={c.id}>
-              <td className="">{c.category}</td>
-              <td className="">{c.price}</td>
+              <td className="Bgc">{c.category}</td>
+              <td className="Bgc">{c.price}</td>
             </tr>
             ))}
             
@@ -87,88 +88,4 @@ export default Categories;
 
 
 
-
-//import axios from "axios";
-// import { ListGroup, Table } from "react-bootstrap";
-// import { useEffect, useState } from "react";
-
-// const Categories = () => {
-//   const [sellerProducts, setSellerProducts] = useState([]);
-
-//   useEffect(() => {
-//     getSellerProducts();
-//   }, []);
-
-//   const normalizeData = (rawData) => {
-//     let sellers = rawData.map((p) => p.seller_id);
-//     let uniqueIds = [...new Set(sellers)];
-//     let sellerProductData = uniqueIds.map((uid) => {
-//       let products = rawData.filter((p) => uid == p.seller_id);
-//       let { first_name, last_name, email, seller_id, category } = products[0];
-//       return {
-//         name: `${first_name} ${last_name}`,
-//         email: email,
-//         products: products,
-//         id: seller_id,
-//         category: category,
-//       };
-//     });
-//     return sellerProductData;
-//   };
-
-//   const renderRows = (products) => {
-//     console.log(products);
-//     return products.map((p) => {
-//       return (
-       
-//         <tr key={p.id}>
-//             <td className="datarow">{p.category}</td>
-            
-//         </tr>
-//       );
-//     });
-//   };
-
-//   const renderData = () => {
-//     return sellerProducts.map((seller) => {
-//       console.log(seller);
-//       return (
-//         <ListGroup.Item key={seller.id}>
-          
-          
-//                 <div className="datarow ">
-//           <Table striped bordered hover >
-       
-            
-//               <tbody>{renderRows(seller.products)}</tbody>
-            
-//           </Table>
-//                 </div>
-//         </ListGroup.Item>
-//       );
-//     });
-//   };
-
-//   const getSellerProducts = async () => {
-//     try {
-//       let res = await axios.get("/api/products");
-//       let normalizedSellerProducts = normalizeData(res.data);
-//       setSellerProducts(normalizedSellerProducts);
-//     } catch (error) {
-//       alert("Error occurred getting products");
-//       console.log(error);
-//     }
-//   };
-//   return (
-//     <div className="Bgc">
-//       <div className="App headbar text-white">
-//         <h1 className="header headbar">Categories</h1>
-//         </div>
-//       <ListGroup>{renderData()}</ListGroup>
-//       {JSON.stringify(sellerProducts)}
-//     </div>
-//   );
-// };
-
-// export default Categories;
 
--- client/src/components/shared/Login.js
+++ client/src/components/shared/Login.js
@@ -42,7 +42,7 @@ const Login = () => {
       {/* <!-- Checkbox --> */}
       <div className="form-check">
         <input  className="form-check-input" type="checkbox" value="" id="form2Example31"  />
-        <label className="form-check-label" for="form2Example31"> Remember me </label>
+        <label className="form-check-label" form="form2Example31"> Remember me </label>
       </div>
     </div>
 
@@ -84,34 +84,3 @@ const Login = () => {
   );
 };
 export default Login;
-
-{
-  /* <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>; */
-}
-// {
-//   <p>email</p>
-//         <input value={email} onChange={(e) => setEmail(e.target.value)} />
-//         <p>password</p>
-//         <input value={password} onChange={(e) => setPassword(e.target.value)} />
-//         <button>login</button>
-//         <hr></hr>
-// }
